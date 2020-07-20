86f4483175428548e5ac6c4a229d2226f4da3a07
Konstantin Dorichev <kdorichev@gmail.com>
2020-07-20 07:43:06 +0300

    __init__.py added 2 make Python aware of submodule

--- common/text/__init__.py
+++ common/text/__init__.py
@@ -12,15 +12,15 @@ _id_to_symbol = {i: s for i, s in enumerate(symbols)}
 _curly_re = re.compile(r'(.*?)\{(.+?)\}(.*)')
 
 
-def text_to_sequence(text: str, cleaner_names) -> list:
+def text_to_sequence(text: str, cleaner_names: list) -> list:
   """Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
 
     The text can optionally have ARPAbet sequences enclosed in curly braces embedded
     in it. For example, "Turn left on {HH AW1 S S T AH0 N} Street."
 
     Args:
-      text: string to convert to a sequence
-      cleaner_names: names of the cleaner functions to run the text through
+      text (str): a string to convert to a sequence
+      cleaner_names (list): a list of the cleaner functions to run the text through
 
     Returns:
       List of integers corresponding to the symbols in the text
