b7bf4f41a7037c2f8057d22673f46ecd13938d83
Konstantin Dorichev <kdorichev@gmail.com>
2020-07-28 15:06:22 +0300

    ffmpeg added to docker image to support flac

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM ${FROM_IMAGE_NAME}
 
 ADD requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install ffmpeg -y
 WORKDIR /workspace/fastpitch
 COPY . .
--- scripts/docker/interactive.sh
+++ scripts/docker/interactive.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --gpus=all -it --rm -e CUDA_VISIBLE_DEVICES -e "TZ=Europe/Moscow" --ipc=host -v $PWD:/workspace/fastpitch/ -v /etc/timezone:/etc/timezone:ro fastpitch:latest bash 
+docker run --gpus=all -it --rm -e CUDA_VISIBLE_DEVICES -e "TZ=Europe/Moscow" --ipc=host -v $PWD:/workspace/fastpitch/ -v /etc/timezone:/etc/timezone:ro fastpitch:ffmpeg bash 
