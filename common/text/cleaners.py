8efc849f61daff195c101eb591e02d2c8b10f9df
Konstantin Dorichev <korichev@gmail.com>
2020-07-28 15:32:18 +0300

    russian_cleaner. Content replaced with the text2speech/text_norm.py

--- common/text/cleaners.py
+++ common/text/cleaners.py
@@ -11,80 +11,181 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
   3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
      the symbols in symbols.py to match your data).
 '''
+__all__ = ['collapse_whitespace', 'lowercase', 'check_no_numbers', 'purge_dots', 'remove_specials',
+           'expand_abbreviations', 'unify_dash_hyphen', 'rm_quot_marks', 'basic_cleaner', 'russian_cleaner']
 
+# Cell
 import re
-from unidecode import unidecode
-from .numbers import normalize_numbers
 
+# Cell
+def collapse_whitespace(text: str) -> str:
+    "Replace multiple various whitespaces with a single space, strip leading and trailing spaces."
 
-# Regular expression matching whitespace:
-_whitespace_re = re.compile(r'\s+')
+    return re.sub(r'[\s\ufeff\u200b\u2060]+', ' ', text).strip()
 
-# List of (regular expression, replacement) pairs for abbreviations:
-_abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
+# Cell
+def lowercase(text: str) -> str:
+    "Convert `text` to lower case."
+
+    return text.lower()
+
+# Cell
+def check_no_numbers(text: str) -> list:
+    "Return a list of digits, or empty list, if not found."
+
+    return re.findall(r"(\d+)", text)
+
+# Internal Cell
+_specials = [(re.compile(f'{x[0]}'), x[1]) for x in [
+    (r'\(?\d\d[:.]\d\d\)?', ''),  # timestamps
+    (r'!\.{1,}', '!'), # !. -> !
+    (r'\?\.{1,}', '?'),# ?. -> ?
+    (r'\/', ''),
+    ]]
+
+# Cell
+def purge_dots(text, purgedots=False):
+    "If `purgedots`, `...`|`…` will be purged. Else replaced with `.`"
+    text = re.sub(r'\s(…)', ' ', text)
+    replacement = '' if purgedots else '.'
+    text = re.sub('…', replacement, text)
+    text = re.sub('\.{3}', replacement, text)
+    text = re.sub('\.{2}', '', text)   # pause .. removed
+    return text
+
+# Cell
+def remove_specials(text: str, purge_digits: bool=None) -> str:
+    "Replace predefined in `_specials` sequence of characters"
+
+    for regex, replacement in _specials:
+        text = re.sub(regex, replacement, text)
+    if purge_digits:
+        text = re.sub('\d', '', text)
+    return text
+
+# Cell
+_abbreviations = [(re.compile(f'\\b{x[0]}', re.IGNORECASE), x[1]) for x in [
+  ('т\.е\.', 'то есть'),
+  ('т\.к\.', 'так как'),
+  ('и т\.д\.', 'и так далее.'),
+  ('и т\.п\.', 'и тому подобное.')
 ]]
 
+# Cell
+def expand_abbreviations(text: str) -> str:
+    "`expand_abbreviations()` defined in `_abbreviations`"
 
-def expand_abbreviations(text):
-  for regex, replacement in _abbreviations:
-    text = re.sub(regex, replacement, text)
-  return text
+    for regex, replacement in _abbreviations:
+        text = re.sub(regex, replacement, text)
+    return text
+
+# Cell
+def unify_dash_hyphen(text: str) -> str:
+    "Unify dash and hyphen symbols -- replace with emdash or hyphen, separate with space."
+
+    text = re.sub('[\u2212\u2012\u2014]', '\u2013', text) # replace minus sign, figure dash, em dash with en dash
+    text = re.sub('[\u2010\u2011]', '\u002d', text)  # hyphen, non-breaking hyphen
+    text = re.sub('\s*?(\u2013)\s*?',' \g<1> ',text)
+    return text
+
+# Cell
+def rm_quot_marks(text: str) -> str:
+    """Remove quotation marks from `text`."""
+    # \u0022\u0027\u00ab\u00bb\u2018\u2019\u201a\u201b\u201c\u201d\u201e\u201f\u2039\u203a\u276e\u276f\u275b\u275c\u275d\u275e\u275f\u2760\u2e42\u301d\u301e\u301f
+    return re.sub('["\'«»‘’‚‛“”„‟‹›❮❯❛❜❝❞❟❠]','',text)
+
+# Cell
+def basic_cleaner(text: str) -> str:
+    "Basic pipeline: lowercase and collapse whitespaces."
+    text = lowercase(text)
+    text = collapse_whitespace(text)
+    return text
+
+# Cell
+def russian_cleaner(text, purge_digits=True, _purge_dots=False):
+    "Pipeline for cleaning Russian text."
+    text = lowercase(text)
+    text = expand_abbreviations(text)
+    text = remove_specials(text, purge_digits=purge_digits)
+    text = purge_dots(text,purgedots=_purge_dots)
+    text = unify_dash_hyphen(text)
+    text = rm_quot_marks(text)
+    text = collapse_whitespace(text)
+    return text
+
+# import re
+# from unidecode import unidecode
+# from .numbers import normalize_numbers
 
 
-def expand_numbers(text):
-  return normalize_numbers(text)
+# # Regular expression matching whitespace:
+# _whitespace_re = re.compile(r'\s+')
+
+# # List of (regular expression, replacement) pairs for abbreviations:
+# _abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
+#   ('mrs', 'misess'),
+#   ('mr', 'mister'),
+#   ('dr', 'doctor'),
+#   ('st', 'saint'),
+#   ('co', 'company'),
+#   ('jr', 'junior'),
+#   ('maj', 'major'),
+#   ('gen', 'general'),
+#   ('drs', 'doctors'),
+#   ('rev', 'reverend'),
+#   ('lt', 'lieutenant'),
+#   ('hon', 'honorable'),
+#   ('sgt', 'sergeant'),
+#   ('capt', 'captain'),
+#   ('esq', 'esquire'),
+#   ('ltd', 'limited'),
+#   ('col', 'colonel'),
+#   ('ft', 'fort'),
+# ]]
 
 
-def lowercase(text):
-  return text.lower()
+# def expand_abbreviations(text):
+#   for regex, replacement in _abbreviations:
+#     text = re.sub(regex, replacement, text)
+#   return text
 
 
-def collapse_whitespace(text):
-  return re.sub(_whitespace_re, ' ', text)
+# def expand_numbers(text):
+#   return normalize_numbers(text)
 
 
-def convert_to_ascii(text):
-  return unidecode(text)
+# def lowercase(text):
+#   return text.lower()
 
 
-def basic_cleaners(text):
-  '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
+# def collapse_whitespace(text):
+#   return re.sub(_whitespace_re, ' ', text)
 
 
-def transliteration_cleaners(text):
-  '''Pipeline for non-English text that transliterates to ASCII.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
+# def convert_to_ascii(text):
+#   return unidecode(text)
 
 
-def english_cleaners(text):
-  '''Pipeline for English text, including number and abbreviation expansion.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
-  text = collapse_whitespace(text)
-  return text
+# def basic_cleaners(text):
+#   '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
+#   text = lowercase(text)
+#   text = collapse_whitespace(text)
+#   return text
+
+
+# def transliteration_cleaners(text):
+#   '''Pipeline for non-English text that transliterates to ASCII.'''
+#   text = convert_to_ascii(text)
+#   text = lowercase(text)
+#   text = collapse_whitespace(text)
+#   return text
+
+
+# def english_cleaners(text):
+#   '''Pipeline for English text, including number and abbreviation expansion.'''
+#   text = convert_to_ascii(text)
+#   text = lowercase(text)
+#   text = expand_numbers(text)
+#   text = expand_abbreviations(text)
+#   text = collapse_whitespace(text)
+#   return text
