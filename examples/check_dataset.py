e0970d05d6ff6e2b676bcf1c7804cc5fc1fcb091
Konstantin Dorichev <korichev@gmail.com>
2020-09-04 11:00:25 +0300

    Initial commit
    Signed-off-by: Konstantin Dorichev <korichev@gmail.com>

---
+++ examples/check_dataset.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Ckeck the dataset:
+    - consistency
+    - vocabulary
+    - accent
+"""
+
+from fastcore.all import *
+from fastai2.data.all import *
+from fastai2_audio.core.all import *
